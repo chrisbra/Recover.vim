a2ccfc7ad4689849e192aa653dde39095b8a8f80
Christian Brabandt <cb@256bit.org>
2010-04-27 09:24:51 +0200

    Added a test for read-only buffers

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
+set -x
 
 for i in "a space" "a\\backslash"  "a
-linebreak" "normal" "a'singlequote" 'a"doublequote'; do
+linebreak" "normal" "a'singlequote" 'a"doublequote' 'readonly'; do
     test -d "$i" && rm -rf "$i"
     mkdir "$i"
     cp recover.sh vimrc_recover "$i"
     pushd "$i"
     ./recover.sh
+    # For readonly commands, this should not 
+    # trigger SwapExists Autocommand
+    if [ "$i" = 'readonly' ]; then
+	echo "This buffer is readonly! The Autocommand SwapExists should not fire!">testfile
+	chmod 0400 testfile
+    fi
     popd
     reset
 done
 for i in "a space" "a\\backslash"  "a
-linebreak" "normal" "a'singlequote" 'a"doublequote'; do
+linebreak" "normal" "a'singlequote" 'a"doublequote' 'readonly'; do
     pushd "$i"
     vim testfile
     popd
